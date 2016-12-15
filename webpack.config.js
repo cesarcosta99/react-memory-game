12290eca11cb7f6af0ee27e628c60091d35f04bf
César Costa <costa.cesar.a@hotmail.com>
2016-12-14 21:56:53 -0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: __dirname + '/src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    preLoaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: 'eslint-loader'}
+    ],
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
+    ]
+  },
+  output: {
+    filename: 'index.bundle.js',
+    path: __dirname + '/dist'
+  },
+  plugins: [HTMLWebpackPluginConfig]
+}
